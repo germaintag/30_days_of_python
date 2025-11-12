d710a0a61b4f2063e6d7e22be99d534d0ba11c59
germaintag <tagodoegermain7@gmail.com>
2025-11-12 01:25:43 +0100

    Initial commit

---
+++ day_1/helloworld.py
@@ -0,0 +1,75 @@
+#Day 1 : 30 days of python
+import sys
+import math
+
+
+#Exercie (Level 1)
+"""
+   This part of the exercise was done in the shell.
+"""
+
+#Exercie (Level 2)
+#Question 1
+print(sys.version)  # Affiche la version de Python
+
+#Question 2
+
+print(6+3)
+print(6-3)
+print(6*3)
+print(6%3)
+print(6**3)
+print(6//3)
+
+#Question 3
+
+print("My first name is: Germain ")
+print("My family name is: TAGODOE")
+print("My country is: TOGO ")
+print("I am enjoying 30 days of python")
+
+#Question 4
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type("Germain"))
+print(type("TAGODOE"))
+print(type("TOGO"))
+
+#Exercie (Level 3)
+#Question 1
+num_one = 1
+num_two = 3.10
+num_three = 4+0j
+the_string = "I'm the GOAT"
+is_end = True
+the_list = ["Koami","Djidoula","Germain"]
+the_tupple = ("Germain ","Germain again ",28,":",5,":",2005)
+the_set = {10,26,"apple", "banana"}
+player = {
+    "full name": "Kylian MBAPPE",
+    "age" :26,
+    "club" : "Real madrid"
+}
+
+#Question 3
+#création des points
+x1,y1 = 2,3
+x2,y2  = 10,8
+distance = math.sqrt(((x2 - x1)**2)+ (y2 - y1))
+print(distance)
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ day_2/variables.py
@@ -0,0 +1,83 @@
+#Day 2 : 30 days of python
+import math
+#Exercices : Level 1
+first_name = "Germain"
+last_name = "TAGODOE"
+full_name = "TAGODOE Germain"
+country = "TOGO"
+city = "Lomé"
+age = 20
+year = 2025
+is_married = False
+is_true = True
+is_light_on = False
+a, b, course = 10, 5.2, "Mathématiques"
+
+#Exercices : Level 2
+#Question 1
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+print(type(a))
+print(type(b))
+print(type(course))
+
+#Question 2
+print(len(first_name))
+
+#Question 3
+print(len(first_name),len(last_name))
+
+#Question 4
+num_one = 5
+num_two = 4
+
+#Question 5
+sum_of_both = num_one + num_two
+
+#Question 6
+diff = num_one - num_two
+
+#Question 7
+product = num_one * num_two
+
+#Question 8
+division  = num_one / num_two
+
+#Question 9
+remainder = num_two %  num_one
+
+#Question 10
+exp = num_one ** num_two
+
+#Question 11
+floor_division = num_one // num_two
+
+#Question 12.i
+area_of_circle = math.pi * (30**2)
+
+#Question 12.ii
+circum_of_circle = 2 * math.pi * 30
+
+#Question 12.iii
+radius = int(input("enter the radius: "))
+area = math.pi * (radius ** 2)
+
+#Question 13
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+country  = input("Enter the name of your country: ")
+age = int(input("Enter your age"))
+
+
+
+
+
+
