938a221cd3548456c8c2df566b8261d8b85a8ba6
germaintag <tagodoegermain7@gmail.com>
2026-04-22 00:07:00 +0200

    Add day 3 exercices: Opérators

---
+++ day_3/operators.py
@@ -0,0 +1,73 @@
+#Day 3: 30 days of python
+#Question 1
+age = 20
+
+#Question 2
+height = 1.77
+
+#Question 3
+complex_number = 20 + 1j
+
+#Question 4
+base = int(input("Enter the base: "))
+the_height = int(input("Enter the hight: "))
+triangle_area = (0.5 * base * the_height)
+print(" The area of the triangle is: ",triangle_area)
+
+#Question 5
+a = int(input("Enter the side a: "))
+b = int(input("Enter the side b: "))
+c = int(input("Enter the side c: "))
+perimeter = a + b + c
+print(" The perimeter of the triangle is: ",perimeter)
+
+#Question 6
+lenght = float(input("Enter the lenght: "))
+width  = float(input("Enter the width: "))
+area = (lenght * width)
+perimeter = (2 * (lenght + width))
+print(" The perimeter of the rectangle is: ",perimeter ,"and the area of the rectangle is: ",area)
+
+#Question 7
+import math
+cercle_radius = float(input("Enter the cercle radius: "))
+pi = math.pi
+area = pi * cercle_radius * cercle_radius
+cercle_circumference = 2 * pi * cercle_radius
+#Question 8
+# calcul the slope, x-intercept and y-intercept of y = 2x -2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
