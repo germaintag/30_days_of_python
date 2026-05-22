f7220eb311739e8839733614f7ee4d8669234fe3
Germain <187400537+germaintag@users.noreply.github.com>
2026-05-22 16:56:37 +0200

    feat: complete Day 3 exercises except Q11 and Q15

--- day_3/operators.py
+++ day_3/operators.py
@@ -1,44 +1,47 @@
 #Day 3: 30 days of python
+#--------------------------------------------------------------------------------------------------
 #Question 1
 age = 20
-
+#--------------------------------------------------------------------------------------------------
 #Question 2
 height = 1.77
-
+#--------------------------------------------------------------------------------------------------
 #Question 3
 complex_number = 20 + 1j
-
+#--------------------------------------------------------------------------------------------------
 #Question 4
 base = int(input("Enter the base: "))
 the_height = int(input("Enter the hight: "))
 triangle_area = (0.5 * base * the_height)
 print(" The area of the triangle is: ",triangle_area)
-
+#--------------------------------------------------------------------------------------------------
 #Question 5
 a = int(input("Enter the side a: "))
 b = int(input("Enter the side b: "))
 c = int(input("Enter the side c: "))
 perimeter = a + b + c
 print(" The perimeter of the triangle is: ",perimeter)
-
+#--------------------------------------------------------------------------------------------------
 #Question 6
 lenght = float(input("Enter the lenght: "))
 width  = float(input("Enter the width: "))
 area = (lenght * width)
 perimeter = (2 * (lenght + width))
 print(" The perimeter of the rectangle is: ",perimeter ,"and the area of the rectangle is: ",area)
-
+#--------------------------------------------------------------------------------------------------
 #Question 7
 import math
 cercle_radius = float(input("Enter the cercle radius: "))
 pi = math.pi
 area = pi * cercle_radius * cercle_radius
 cercle_circumference = 2 * pi * cercle_radius
+#--------------------------------------------------------------------------------------------------
 #Question 8
 # calculate the  slope, x-intercept and y-intercept of y = 2x -2
 slope = 2
 y_intercept = -2
 x_intercept =  y_intercept / slope
+#--------------------------------------------------------------------------------------------------
 #Question 9
 """
 Calculate the slope and Euclidean distance betwenn point (2, 2) and point (6,10)
@@ -46,6 +49,7 @@ Calculate the slope and Euclidean distance betwenn point (2, 2) and point (6,10)
 """
 slope_2 = (10 - 2)/(6 - 2)
 euclidean_distance = math.sqrt(pow(6-2,2) + pow(10-2,2))
+#--------------------------------------------------------------------------------------------------
 #Question 10
 #1 check if both slope are equal
 are_equal = slope == slope_2
@@ -56,8 +60,12 @@ print("Is the first slope greater than slope 9?", is_first_slope_greater)
 #3 check if the second slope is greater than the first slope
 is_second_slope_greater = slope < slope_2
 print("Is the second slope greater than slope 8?", is_second_slope_greater)
+#--------------------------------------------------------------------------------------------------
 #Question 11
-
+"""
+TODO
+"""
+#--------------------------------------------------------------------------------------------------
 #Question 12
 """
 Calculate the length of the words "python" and "dragon",then make a falsy comparison to check if their lengths are different.
@@ -72,6 +80,65 @@ print("the lenght of the word dragon is :",len(second_word))
 # Comparaison falsy : check if their lenght are different
 falsy_comparison = length_python != length_dragon
 print(falsy_comparison)
+#--------------------------------------------------------------------------------------------------
+#Question 13
+"""
+Check if "on" is found in both 'python' and 'dragon'
+"""
+first_word = "python"
+second_word = "dragon"
+result = "on" in first_word and second_word == "dragon"
+print(result)
+#--------------------------------------------------------------------------------------------------
+#Question 14
+phrase = "I hope this course is not full of jargon."
+the_result = "jargon" in phrase
+print(the_result)
+#--------------------------------------------------------------------------------------------------#--------------------------------------------------------------------------------------------------
+#Question 15
+"""
+"""
+#--------------------------------------------------------------------------------------------------#--------------------------------------------------------------------------------------------------
+#Question 16
+"""
+Find the length of the text python and convert the value to float and convert it to string
+"""
+text_length = len("python")
+print(text_length)
+text_length_in_to_float = float(text_length)
+text_length_in_to_string = str(text_length_in_to_float)
+print(text_length_in_to_float)
+print(text_length_in_to_string)
+#--------------------------------------------------------------------------------------------------
+#Question 17
+"""
+check if the number is divisible by 2
+"""
+le_nombre = int(input("Enter the number:"))
+is_divisible = le_nombre % 2 == 0
+print(is_divisible)
+#--------------------------------------------------------------------------------------------------
+#Question 18
+"""
+Check if the floor division of 7 by 3 is equal to the int converted value of 2.7
+"""
+floor_division = 7//3
+number_0 = int(2.7)
+print(floor_division is number_0)
+#--------------------------------------------------------------------------------------------------
+#Question 19
+"""
+Check if type of '10' is equal to type of 10
+"""
+the_compared = "on"
+the_comparing_element =  10
+print(the_compared is the_comparing_element)
+#--------------------------------------------------------------------------------------------------
+#Question 20
+first_number = int(9.8)
+second_number = 10
+print( first_number is second_number )
+#--------------------------------------------------------------------------------------------------
 #Question 21
 """
 Calculate the pay of the person based on hours worked and hourly rate.
@@ -80,6 +147,7 @@ hours = int(input("Enter the hours: "))
 rate_per_hour = float(input("Enter the rate per hours: "))
 weekly_earning = hours * rate_per_hour
 print("the weekly earning is :" ,weekly_earning)
+#--------------------------------------------------------------------------------------------------
 #Question 22
 """
 Calculate the number of seconds a person can live.
@@ -88,7 +156,8 @@ number_of_year_lived = int (input("Enter the number of years lived: "))
 #31536000 correspond to the numbers of second in a  year
 number_of_second_lived = number_of_year_lived * 31536000
 print("You have lived for ",number_of_second_lived, " seconds")
-#Question 22
+#--------------------------------------------------------------------------------------------------
+#Question 23
 """
 Python script that displays the following table
 1 1 1 1 1
