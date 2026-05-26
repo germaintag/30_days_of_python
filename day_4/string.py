5a16e562f89a76b7fa239eabf02da2ca427bc0a2
Germain <187400537+germaintag@users.noreply.github.com>
2026-05-26 17:42:59 +0200

    feat: complete partial exercises for Day 4

---
+++ day_4/string.py
@@ -0,0 +1,55 @@
+#Day 4: 30 days of python
+#--------------------------------------------------------------------------------------------------
+#Question 1
+"""
+Concatenate the string 'Thirty', 'Days', 'Of', 'Python' to a single string, 'Thirty Days Of Python'.
+"""
+
+space = " "
+first_word = 'Thirty'
+second_word = 'Days'
+third_word =  'Of'
+fourth_word = 'Python'
+print(first_word + space + second_word + space + third_word + space + fourth_word)
+
+#--------------------------------------------------------------------------------------------------
+#Question 2
+"""
+Concatenate the string 'Coding', 'For' , 'All' to a single string, 'Coding For All'.
+"""
+space = " "
+first_word = 'Coding'
+second_word = 'For'
+third_word = 'All'
+print(first_word + space + second_word + space + third_word)
+#--------------------------------------------------------------------------------------------------
+#Question 3
+"""Declare a variable named company and assign it to an initial value "Coding For All"."""
+company = "Coding For All"
+#--------------------------------------------------------------------------------------------------
+#Question 4
+"""Print the variable company using print()"""
+print(company)
+#--------------------------------------------------------------------------------------------------
+#Question 5
+"""Print the length of the company string using len() method and print()"""
+print(len(company))
+#--------------------------------------------------------------------------------------------------
+#Question 6
+"""Change all the characters to uppercase letters using upper() method."""
+company = company.upper()
+#--------------------------------------------------------------------------------------------------
+#Question 7
+"""Change all the characters to lowercase letters using lower() method."""
+company = company.lower()
+
+
+
+
+
+
+
+
+
+
+
