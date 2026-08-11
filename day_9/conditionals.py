c64b0f2cc211f3be6f87c895ded96b1394ea3c26
Germain <187400537+germaintag@users.noreply.github.com>
2026-08-12 00:38:22 +0200

    WIP: Day 9 exercises (Level 1 and 2 completed)

--- day_9/conditionals.py
+++ day_9/conditionals.py
@@ -1,14 +1,17 @@
 #Day 9: 30 days of python
 #--------------------------------------------------------------------------------------------------
 #Exercises: Level 1
+#--------------------------------------------------------------------------------------------------
 #Question 1
+#--------------------------------------------------------------------------------------------------
 user_age = int(input("Enter your age:"))
 if user_age >= 18:
     print("You are old enough to learn to drive.")
 else:
     print("you need "+ str(18 - user_age) +" more years to learn to drive." )
+#--------------------------------------------------------------------------------------------------
 #Question 2 Must be completed
-
+#--------------------------------------------------------------------------------------------------
 #Question 3
 a = int (input("Enter the first number:"))
 b = int (input("Enter the second number:"))
@@ -20,6 +23,7 @@ else:
     print( a , "is equal to b")
 #--------------------------------------------------------------------------------------------------
 #Exercises: Level 2
+#--------------------------------------------------------------------------------------------------
 #Question 1: code which gives grade to students according to theirs scores
 score = int(input("Enter you score: "))
 if 0 <= score <= 59:
@@ -32,7 +36,7 @@ elif 80 <= score <= 89:
     print("Your grade is B")
 elif 90 <= score <= 100:
     print("Your grade is A")
-
+#--------------------------------------------------------------------------------------------------
 #Question 2
 month = str(input("Enter your month:"))
 if month == "September"or month == "October" or month == "November":
@@ -43,6 +47,7 @@ elif month == "March"or month == "Avril" or month == "May":
     print("the season is spring.")
 elif month == "June"or month == "July" or month == "August":
     print("the season is summer.")
+#--------------------------------------------------------------------------------------------------
 #Question 3
 fruits = ['banana', 'orange', 'mango', 'lemon']
 new_fruit = str(input("Enter the new fruit:"))
@@ -53,6 +58,4 @@ else:
 print(fruits)
 #--------------------------------------------------------------------------------------------------
 #Exercises: Level 3
-#Question 1
-#Question 2
-#Question 3
+#--------------------------------------------------------------------------------------------------
